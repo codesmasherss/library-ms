c3bc66a0055c186c5fd503e7e7d263d4b6833b3d
Mohammad Nabi <mnabimd@gmail.com>
2022-11-23 00:24:28 +0430

    fake saveCredentials function added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+//  Login script
+function saveUserCredentials() {
+   // Username
+   let username = document.getElementById('username').value;
+   let password = document.getElementById('password').value;
 
+   if (username.length < 8 || !username) {
+      alert('Please enter a username and should be greater than 8 chars!');
+      return false;
+   } else if (password.length < 6 || !password) {
+      alert('Please enter a password and should be greater than 6 chars!');
+   } else {
+      window.location.href = '/';
+   }
+}
